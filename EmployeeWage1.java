dea7c15c7ef8b671b6ebd5eac73ddb36604597ac
mohanbendegumbali <mohan0311@hotmail.com>
2023-01-08 19:13:22 +0530

    Added Employee Wage

---
+++ EmployeeWage1.java
@@ -0,0 +1,21 @@
+public class EmployeeWage1 {
+    static final int WAGE_PER_HOUR = 20;
+    static final int FULL_DAY_HOUR = 8;
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Employee Wage");
+        int employeeCheck = (int) (Math.random() * 10) % 2;
+        System.out.println(employeeCheck);
+        int dailyWage = 0;
+        if (employeeCheck == 1) {
+            System.out.println("Employee is Present");
+            dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+        } else {
+            System.out.println("Employee is Absent");
+        }
+        System.out.println("Daily wage => " + dailyWage);
+    }
+}
+
+
