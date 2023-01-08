d5982975afeaca12742ce8f1dba5810ead1f73ce
mohanbendegumbali <mohan0311@hotmail.com>
2023-01-08 19:33:10 +0530

    Added Employee Wage

--- EmployeeWage1.java
+++ EmployeeWage1.java
@@ -1,20 +1,46 @@
-public class EmployeeWage1 {
+public class EmployeeWage1{
+
     static final int WAGE_PER_HOUR = 20;
-    static final int FULL_DAY_HOUR = 8;
+    static final int FULL_DAY_HOUR = 12;
+
+    static final int PART_TIME_HOUR = 7;
+
+    static final int WORKING_DAYS_PER_MONTH = 20;
+
+    static final int WORKING_HOURS_PER_MONTH = 100;
+
+    static final int IS_PART_TIME = 1;
+
+    static final int IS_FULL_TIME = 2;
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to Employee Wage");
-        int employeeCheck = (int) (Math.random() * 10) % 2;
-        System.out.println(employeeCheck);
-        int dailyWage = 0;
-        if (employeeCheck == 1) {
-            System.out.println("Employee is Present");
-            dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-        } else {
-            System.out.println("Employee is Absent");
+
+        int dayCount = 1;
+        int workingHrs = 0;
+        while (dayCount <= WORKING_DAYS_PER_MONTH && workingHrs < WORKING_HOURS_PER_MONTH ) {
+            int employeeCheck = (int) (Math.random() * 10) % 3;
+            System.out.println(employeeCheck);
+            int dailyWage = 0;
+            switch (employeeCheck) {
+                case IS_PART_TIME:
+                    System.out.println("Employee is Part Time");
+                    dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+                    workingHrs = workingHrs + PART_TIME_HOUR;
+                    break;
+                case IS_FULL_TIME:
+                    System.out.println("Employee is Full Time");
+                    dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                    workingHrs = workingHrs + FULL_DAY_HOUR;
+                    break;
+                default:
+                    System.out.println("Employee is Absent");
+            }
+            System.out.println("Daily wage of Day => " + dayCount + " is "  + dailyWage);
+            System.out.println("working hrs => " + workingHrs);
+            dayCount++;
         }
-        System.out.println("Daily wage => " + dailyWage);
     }
 }
 
